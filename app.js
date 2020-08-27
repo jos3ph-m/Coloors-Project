686cdc870daec7869b2525ee478b7a4c31cf9713
Joseph M <jmedina1985@hotmail.com>
2020-08-26 21:05:11 -0700

    Check text contrast

--- app.js
+++ app.js
@@ -20,7 +20,19 @@ function randomColors() {
     // Add color to background
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+
+    // Check text contrast
+    checkTextContrast(randomColor, hexText);
   });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
 }
 
 randomColors();
